f73c20a2ad39346c4b02536de3ab1662d6242d37
luismibm <luibenmor@gmail.com>
2024-10-05 16:16:39 +0200

    Finished: Ex3RandomAcces

--- UD1-PersistenciaDeFicheros/PersistenciaEnObjetos/src/Ex3RandomAcces.java
+++ UD1-PersistenciaDeFicheros/PersistenciaEnObjetos/src/Ex3RandomAcces.java
@@ -1,13 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex3RandomAcces {
 
     public static void main(String[] args) throws IOException {
         double num;
-        boolean fileEnd = false;
+        boolean exit = false;
         try {
             RandomAccessFile file = new RandomAccessFile("test.txt", "rw");
             do {
@@ -17,37 +18,67 @@ public class Ex3RandomAcces {
                 System.out.println("3 - showFileCreated");
                 System.out.println("4 - replaceNumber");
                 System.out.println("5 - Exit");
-                System.out.print("Option:");
+                System.out.print("Option: ");
                 Scanner sc = new Scanner(System.in);
-                int option = sc.nextInt();
-                switch (option) {
-                    case 1:
-                        num = sc.nextDouble();
-                        file.seek(0);
-                        file.writeDouble(num);
-                        break;
-                    case 2:
-                        // Añadir double al final del fichero
-                        break;
-                    case 3:
-                        file.seek(0);
-                        while (file.getFilePointer() < file.length()) {
-                            System.out.println(file.readDouble());
-                        }
-                        break;
-                    case 4:
-                        // Sustituir el número indicado por el usuario por otro numero también indicado pro el usuario
-                        break;
-                    case 5:
-                        System.out.println("\nShutting Down...\n");
-                        fileEnd = true;
-                        break;
-                    default:
-                        System.out.println("\nSelect a valid option\n");
-                        break;
+                try {
+                    int option = sc.nextInt();
+                    switch (option) {
+                        case 1:
+                            try {
+                                System.out.print("- Number: ");
+                                num = sc.nextDouble();
+                                file.seek(0);
+                                file.writeDouble(num);
+                            } catch (InputMismatchException e) {
+                                System.out.println("ERROR: InputMismatchException");
+                            }
+                            break;
+                        case 2:
+                            try {
+                                System.out.print("- Number: ");
+                                num = sc.nextDouble();
+                                file.seek(file.length());
+                                file.writeDouble(num);
+                            } catch (InputMismatchException e) {
+                                System.out.println("ERROR: InputMismatchException");
+                            }
+                            break;
+                        case 3:
+                            file.seek(0);
+                            int cont = 0;
+                            System.out.println();
+                            while (file.getFilePointer() < file.length()) {
+                                System.out.println("[" + cont + "]: " + file.readDouble());
+                                cont++;
+                            }
+                            System.out.println();
+                            break;
+                        case 4:
+                            try {
+                                System.out.print("- Position: ");
+                                long pos = sc.nextLong();
+                                System.out.print("- Number: ");
+                                num = sc.nextDouble();
+                                file.seek(pos * 8);
+                                file.writeDouble(num);
+                            } catch (InputMismatchException e) {
+                                System.out.println("ERROR: InputMismatchException");
+                            }
+                            break;
+                        case 5:
+                            System.out.println("\nShutting Down...\n");
+                            exit = true;
+                            sc.close();
+                            break;
+                        default:
+                            System.out.println("\nSelect a valid option\n");
+                            break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("ERROR: InputMismatchException");
                 }
 
-            } while (!fileEnd);
+            } while (!exit);
             file.close();
         } catch (FileNotFoundException e) {
             System.out.println("ERROR: File Not Found");
